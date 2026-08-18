f887e7b277bfedbc07398a36625a87fe9753a0db
gloriacampossilva673-prog <gloriacampossilva673@gmail.com>
2026-08-18 16:13:43 -0300

    Assunto 2
    Inclusão de material.

---
+++ Assunto 2 - Protocolo HTTP/1_cadastro_status.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastro - Status code</title>
+</head>
+<body>
+  
+    <h1>Cadastro de Aluno (com status code)</h1>
+    <form action="" method="post">
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" required><br><br>
+
+        <label for="idade">Idade:</label>
+        <input type="text" name="idade" required><br><br>
+
+        <button type="submit">Enviar</button>
+
+    </form>
+</body>
+</html>
